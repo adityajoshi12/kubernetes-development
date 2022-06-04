627206cb339751faac5c3c6d762e789d51c6dbf0
adityajoshi12 <adityaprakashjoshi1@gmail.com>
2022-06-04 10:53:18 +0530

    added pod delete notification controller
    Signed-off-by: adityajoshi12 <adityaprakashjoshi1@gmail.com>

--- k8-go-client/service-discovery/go.mod
+++ k8-go-client/service-discovery/go.mod
@@ -1,3 +1,4 @@
 module service-discovery
 
 go 1.18
+require k8s.io/client-go v0.24.1
--- k8-go-client/service-discovery/main.go
+++ k8-go-client/service-discovery/main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+	"time"
+)
+
 func main() {
 
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Errorf(err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	factory := informers.NewSharedInformerFactory(clientset, 10*time.Second)
+
+	informer := factory.Networking().V1().Ingresses().Informer()
+	stopper := make(chan struct{})
+	defer close(stopper)
+	defer runtime.HandleCrash()
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			//TODO Enable registration
+		},
+	})
+
+	go informer.Run(stopper)
+
+	<-stopper
 }
