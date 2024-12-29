1bda74d63c24ce5b30472638d2990bdb2df25818
adityajoshi12 <adityaprakashjoshi1@gmail.com>
2024-12-29 09:13:35 +0530

    added github actions for the devenv operator

--- devenv-operator/start.sh
+++ devenv-operator/start.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-#kind create cluster --config kind-config.yaml
-#sleep 10
-
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-
-#kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/refs/heads/main/deploy/static/provider/do/deploy.yaml
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.16.2/cert-manager.yaml
-
-#kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type=json -p='[{"op": "remove", "path": "/spec/template/spec/tolerations"}]'
-#kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type=json -p='[{"op": "remove", "path": "/spec/template/spec/nodeSelector"}]'
-#
-#
-#echo "127.0.0.1 echoserver.local" | sudo tee -a /etc/hosts
+# when running in KindCluster
+if [ -z "$1" ]; then
+  kind create cluster --config kind-config.yaml
+  sleep 10
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+  kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type=json -p='[{"op": "remove", "path": "/spec/template/spec/tolerations"}]'
+  kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type=json -p='[{"op": "remove", "path": "/spec/template/spec/nodeSelector"}]'
+else
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/refs/heads/main/deploy/static/provider/do/deploy.yaml
+  kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.16.2/cert-manager.yaml
+fi
